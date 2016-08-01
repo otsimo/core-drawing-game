a4b00badce8f5ef25717a0fa9d8e2ca05fe8873c
Ünal Akünal <unal@otsimo.com>
2016-08-01 15:41:08 +0300

    update version

--- src/js/states/boot.js
+++ src/js/states/boot.js
@@ -10,17 +10,12 @@ export default class Boot extends Phaser.State {
         this.game.input.maxPointers = 1;
         this.game.stage.backgroundColor = otsimo.kv.loading_screen.background_color;
 
-        if (this.game.device.desktop) {
+        /*if (this.game.device.desktop) {
             this.game.scale.pageAlignHorizontally = true;
         } else {
-            this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-            this.game.scale.minWidth = 1024;
-            this.game.scale.minHeight = 768;
-            this.game.scale.maxWidth = 1024;
-            this.game.scale.maxHeight = 768;
             this.game.scale.forceOrientation(true);
             this.game.scale.pageAlignHorizontally = true;
-        }
+        }*/
         this.game.state.start('Preloader');
     }
 }
